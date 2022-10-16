c8a124e80690ea67277421a3644ccf5f41633a73
mahedi <mahedianik4@gmail.com>
2022-10-16 13:53:00 +0600

    add mongodb connet srv

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 function dbConnect(){
-  mongoose.connect('mongodb+srv://Job-Portal:ujB5KCBCKrTJ3NQe@cluster0.bdcmpkm.mongodb.net/?retryWrites=true&w=majority');
+  
+    mongoose.connect(`mongodb+srv://tourAdmin:RIucOBwpcqnZASdR@cluster0.orvnefn.mongodb.net/?retryWrites=true&w=majority`)
+    .then()
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 module.exports = dbConnect;
