e863af52a06414752a2c2ee2fb354bec1a0bba7f
mahedi <mahedianik4@gmail.com>
2022-10-16 13:31:12 +0600

    add mongodb connet srv

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function dbConnect(){
-  mongoose.connect('mongodb://localhost:27017/inventory-management');
+  mongoose.connect('mongodb+srv://Job-Portal:ujB5KCBCKrTJ3NQe@cluster0.bdcmpkm.mongodb.net/?retryWrites=true&w=majority');
 }
 
 module.exports = dbConnect;
